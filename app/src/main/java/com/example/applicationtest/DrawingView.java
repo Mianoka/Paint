319276cd664f24f4be992c2c5d51a503d4de80d6
Mianoka Fitia RANARIJAONA <mianoka.fitia.ranarijaona@esti.mg>
2022-08-08 17:12:21 +0000

    Update DrawingView.java

--- app/src/main/java/com/example/applicationtest/DrawingView.java
+++ app/src/main/java/com/example/applicationtest/DrawingView.java
@@ -8,72 +8,151 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-import androidx.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
+
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 
 public class DrawingView extends View {
-
+    private Paint paint = new Paint();
+    public int color = Color.BLACK, colorBorder = Color.BLACK;
     int touchX = -1, touchY = -1, firstTouchX = -1, firstTouchY = -1;
-    int color;
-    public List<Coord> liste = new ArrayList();
-    private Paint paint;
-    ArrayList<Integer> currentX = new ArrayList();
-    ArrayList<Integer> currentY = new ArrayList();
-    ArrayList<Integer> currentColor = new ArrayList();
+    public int border = 10;
+    public String formeType = "Line";
 
+    private List <Integer> FreeX = new ArrayList<Integer>();
+    private List <Integer> FreeY = new ArrayList<Integer>();
+    private Coord touch = null;
+
+    private List <Coord> Touchs = new ArrayList<>();
 
     public DrawingView(Context context){
+
         super(context);
     }
 
     public DrawingView(Context context, @Nullable AttributeSet attrs) {
+
         super(context, attrs);
+    }
+
+    private void drawingShape(Canvas canvas, int left, int top, int right, int bottom, String formeType, Paint paint) throws Exception {
+        switch(formeType){
+            case "Rect":
+                canvas.drawRect(left, top, right, bottom, paint);
+                break;
+            case "Oval":
+                canvas.drawOval(left, top, right, bottom, paint);
+                break;
+            case "Line":
+                canvas.drawLine(left + 100, top + 100, right - 100, bottom - 100, paint);
+                break;
+            default:
+                throw new Exception("Unrecognizable shape");
+        }
+    }
+
+    private void drawing(Canvas canvas, int left, int top, int right, int bottom, int border, int color, int colorBorder, String formeType) throws Exception {
+        Paint paintToDraw = this.paint;
+        paintToDraw.setStyle(Paint.Style.FILL);
+        paintToDraw.setColor(color);
+        drawingShape(canvas, left, top, right, bottom, formeType, paintToDraw);
+        paintToDraw.setStyle(Paint.Style.STROKE);
+        paintToDraw.setColor(colorBorder);
+        paintToDraw.setStrokeWidth(border);
+        drawingShape(canvas, left, top, right, bottom, formeType, paintToDraw);
+    }
+
+    private void drawing(Canvas canvas, List<Integer> X, List<Integer> Y, int border, int color){
+        this.paint.setStrokeWidth(border);
+        this.paint.setColor(color);
+        for(int i=0; i<X.size(); i++){
+            int pointX = X.get(i);
+            int pointY = Y.get(i);
+            canvas.drawPoint(pointX, pointY, this.paint);
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
         super.onDraw(canvas);
 
-        for(int i=0; i<liste.size(); i++){
-            paint.setColor(liste.get(i).getColor());
-            canvas.drawRect(liste.get(i).getxTop() - 250, liste.get(i).getyBottom() - 250, liste.get(i).getxLeft() + 250, liste.get(i).getyRight() + 250, liste.get(i).getPaint());
+        Touchs = Coord.getTouchs();
+
+        for(int i=0; i<Touchs.size(); i++){
+            Coord toDraw = Touchs.get(i);
+            if(toDraw.getType().equals("Free")){
+                drawing(canvas, toDraw.getxList(), toDraw.getyList(), toDraw.getBorder(), toDraw.getColor());
+            }else{
+                try {
+                    drawing(canvas,
+                            toDraw.getLeft() - 100,
+                            toDraw.getTop() - 100,
+                            toDraw.getRight() + 100,
+                            toDraw.getBottom() + 100,
+                            toDraw.getBorder(),
+                            toDraw.getColor(),
+                            toDraw.getColorBorder(),
+                            toDraw.getType());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
 
-        paint = new Paint();
-        paint.setColor(color);
-
-        if(touchX != -1 && touchY != -1 ) {
-            canvas.drawRect(firstTouchX - 250, firstTouchY - 250, touchX + 250, touchY + 250, paint);
+        if(touchX!=-1 && touchY != -1){
+            if (this.formeType.equals("Free")){
+                drawing(canvas, FreeX, FreeY, this.border, this.colorBorder);
+            }else{
+                try {
+                    drawing(canvas,
+                            firstTouchX - 100,
+                            firstTouchY - 100,
+                            touchX + 100,
+                            touchY + 100,
+                            this.border,
+                            this.color,
+                            this.colorBorder,
+                            this.formeType);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
-        if(touchX == -1){
+        if(touchX==-1){
             firstTouchX = (int) event.getX();
             firstTouchY = (int) event.getY();
         }
 
-        touchX = (int) event.getX();
-        touchY = (int) event.getY();
+        if(formeType.equals("Free")){
+            FreeX.add((int) event.getX());
+            FreeY.add((int) event.getY());
+            if (touch == null){
+                touch = new Coord(FreeX, FreeY, border, colorBorder, paint);
+            }else{
+                touch.addToFreeLists((int) event.getX(), (int) event.getY());
+            }
+            if(event.getAction() == MotionEvent.ACTION_UP){
+                FreeX.clear();
+                FreeY.clear();
+                touch = null;
+                touchX = touchY = -1;
+            }
+        }else{
+            touchX = (int) event.getX();
+            touchY = (int) event.getY();
 
-        Random rnd = new Random();
-        color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-
-        if(event.getAction() == MotionEvent.ACTION_UP){
-
-            Coord coord = new Coord(firstTouchX, firstTouchY, touchX, touchY, color, paint);
-            liste.add(coord);
-            touchX = -1;
-            touchY = -1;
+            if(event.getAction() == MotionEvent.ACTION_UP){
+                new Coord(firstTouchX, firstTouchY, touchX, touchY, border, color, colorBorder, formeType, paint);
+                touchX = touchY = -1;
+            }
         }
 
         invalidate();
@@ -81,5 +160,5 @@ public class DrawingView extends View {
         return true;
     }
 
-
 }
+
